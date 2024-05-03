3ec8e72745baa40be56d53830d2c66b767a25ec5
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-03 12:22:24 +0200

    MOR-2: localhost on develop profile works

---
+++ src/main/java/en/ratings/own/my/Configuration.java
@@ -0,0 +1,5 @@
+package en.ratings.own.my;
+
+public final class Configuration {
+    public static final String PROFILE_DEV = "dev";
+}
--- src/main/java/en/ratings/own/my/MyOwnRatingsApplication.java
+++ src/main/java/en/ratings/own/my/MyOwnRatingsApplication.java
@@ -1,13 +1,18 @@
 package en.ratings.own.my;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import static en.ratings.own.my.Configuration.PROFILE_DEV;
 
 @SpringBootApplication
 public class MyOwnRatingsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyOwnRatingsApplication.class, args);
+        new SpringApplicationBuilder()
+                .profiles(PROFILE_DEV)
+                .sources(MyOwnRatingsApplication.class)
+                .run(args);
     }
 
 }
