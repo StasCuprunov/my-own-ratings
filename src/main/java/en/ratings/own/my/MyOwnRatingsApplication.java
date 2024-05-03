16c6e5891482f6d0057890df0249df0710db9f62
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-03 10:58:23 +0200

    MOR-0: create project

---
+++ src/main/java/en/ratings/own/my/MyOwnRatingsApplication.java
@@ -0,0 +1,13 @@
+package en.ratings.own.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnRatingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOwnRatingsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/en/ratings/own/my/MyOwnRatingsApplicationTests.java
@@ -0,0 +1,13 @@
+package en.ratings.own.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnRatingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
