bbc719b48074efbc8c69f787881292ce45c19f2a
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-04 11:23:56 +0200

    MOR-5: adapt repository username to email

--- src/main/java/en/ratings/own/my/repository/UserRepository.java
+++ src/main/java/en/ratings/own/my/repository/UserRepository.java
@@ -4,6 +4,6 @@ import en.ratings.own.my.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, Long> {
-    User findByUsername(String username);
+    User findByEmail(String email);
     <S extends User> S save(S user);
 }
