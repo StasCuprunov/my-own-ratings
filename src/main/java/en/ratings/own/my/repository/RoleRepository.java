343cd14a68e08b0f47b3ec5b377082e3555bb918
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-04 11:27:54 +0200

    MOR-5: create RoleRepository

---
+++ src/main/java/en/ratings/own/my/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package en.ratings.own.my.repository;
+
+import en.ratings.own.my.model.role.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends MongoRepository<Role, Long> {
+
+    Optional<Role> findById(Long id);
+
+    <S extends Role> S save(S role);
+}
