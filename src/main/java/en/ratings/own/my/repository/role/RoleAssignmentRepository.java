efb36c299563ececd5cb71edb154929639ea2fda
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-05 15:01:52 +0200

    MOR-5: annotate repositories

--- src/main/java/en/ratings/own/my/repository/role/RoleAssignmentRepository.java
+++ src/main/java/en/ratings/own/my/repository/role/RoleAssignmentRepository.java
@@ -2,10 +2,12 @@ package en.ratings.own.my.repository.role;
 
 import en.ratings.own.my.model.role.RoleAssignment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface RoleAssignmentRepository extends MongoRepository<RoleAssignment, Long> {
     Optional<List<RoleAssignment>> findAllByUserId(Long userId);
 
--- src/main/java/en/ratings/own/my/repository/role/RoleRepository.java
+++ src/main/java/en/ratings/own/my/repository/role/RoleRepository.java
@@ -2,9 +2,10 @@ package en.ratings.own.my.repository.role;
 
 import en.ratings.own.my.model.role.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface RoleRepository extends MongoRepository<Role, Long> {
 
     Optional<Role> findById(Long id);
