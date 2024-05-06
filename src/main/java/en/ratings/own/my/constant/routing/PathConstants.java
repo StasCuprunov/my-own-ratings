3bb80e557bee365ed1cc7b02ef0d47fd4aedd1b9
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-06 10:56:09 +0200

    MOR-5: define backend routing paths

---
+++ src/main/java/en/ratings/own/my/constant/routing/PathConstants.java
@@ -0,0 +1,12 @@
+package en.ratings.own.my.constant.routing;
+
+public class PathConstants {
+
+    public static final String CREATE_PATH = "/create";
+
+    public static final String ID_PATH = "/id";
+
+    public static final String EDIT_PATH = "/edit" + ID_PATH;
+
+    public static final String DELETE_PATH = "/delete" + ID_PATH;
+}
---
+++ src/main/java/en/ratings/own/my/constant/routing/RoutingConstants.java
@@ -0,0 +1,39 @@
+package en.ratings.own.my.constant.routing;
+
+import static en.ratings.own.my.constant.routing.PathConstants.CREATE_PATH;
+import static en.ratings.own.my.constant.routing.PathConstants.DELETE_PATH;
+import static en.ratings.own.my.constant.routing.PathConstants.EDIT_PATH;
+import static en.ratings.own.my.constant.routing.PathConstants.ID_PATH;
+
+public class RoutingConstants {
+
+    public static final String ROUTING_DEFAULT = "/";
+
+    public static final String ROUTING_LOGIN = "/login";
+
+    public static final String ROUTING_LOGOUT = "/logout";
+
+    public static final String ROUTING_START_PAGE = "/start-page";
+
+    public static final String ROUTING_USER = "/user";
+
+    public static final String ROUTING_USER_CREATION = ROUTING_USER + CREATE_PATH;
+
+    public static final String ROUTING_RATING = "/rating";
+
+    public static final String ROUTING_RATING_ID = ROUTING_RATING + ID_PATH;
+
+    public static final String ROUTING_RATING_CREATE = ROUTING_RATING + CREATE_PATH;
+
+    public static final String ROUTING_RATING_EDIT = ROUTING_RATING + EDIT_PATH;
+
+    public static final String ROUTING_RATING_DELETE = ROUTING_RATING + DELETE_PATH;
+
+    public static final String ROUTING_RATING_ENTRY = "/rating-entry";
+
+    public static final String ROUTING_RATING_ENTRY_CREATE = ROUTING_RATING_ENTRY + CREATE_PATH;
+
+    public static final String ROUTING_RATING_ENTRY_EDIT  = ROUTING_RATING_ENTRY + EDIT_PATH;
+
+    public static final String ROUTING_RATING_ENTRY_DELETE = ROUTING_RATING_ENTRY + DELETE_PATH;
+}
