3cfdbc4de5fe8221dcc1beb1b369b2d34614094a
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-10 14:30:40 +0200

    MOR-7: implement test for RoleRepositoryService if every role is unique in database

---
+++ src/test/java/en/ratings/own/my/test/integration/service/RoleRepositoryServiceIntegrationTest.java
@@ -0,0 +1,48 @@
+package en.ratings.own.my.test.integration.service;
+
+import en.ratings.own.my.AbstractIntegrationTest;
+import en.ratings.own.my.enums.RoleEnum;
+import en.ratings.own.my.model.role.Role;
+import en.ratings.own.my.service.repository.role.RoleRepositoryService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class RoleRepositoryServiceIntegrationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private RoleRepositoryService roleRepositoryService;
+
+    @Test
+    public void testIfAllRolesFromEnumAreExistentAndUnique() {
+        ArrayList<Role> roles = roleRepositoryService.findAll();
+        ArrayList<String> errorMessages = new ArrayList<>();
+        for (RoleEnum roleEnum: RoleEnum.values()) {
+            String name = roleEnum.name();
+            int numberOfEntriesFound = 0;
+            for (Role role: roles) {
+                if (role.getName().equals(name)) {
+                    numberOfEntriesFound++;
+                }
+            }
+            if (numberOfEntriesFound != 1) {
+                errorMessages.add(errorMessage(name, numberOfEntriesFound));
+            }
+        }
+
+        if (!errorMessages.isEmpty()) {
+            System.out.println("The role repository is not valid because:");
+            for (String errorMessage: errorMessages) {
+                System.out.println(errorMessage);
+            }
+        }
+        assertThat(errorMessages.isEmpty()).isTrue();
+    }
+
+    private String errorMessage(String roleName, int numberOfEntriesFound) {
+        return "Role with name " + roleName + " has " + numberOfEntriesFound + "entries in database.";
+    }
+}
