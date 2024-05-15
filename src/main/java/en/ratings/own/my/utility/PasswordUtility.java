8018676919ed5852e1c51fd484a9a7b2c091e57f
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-15 12:11:12 +0200

    MOR-32: fix password regex, fix UserControllerIntegrationTest

--- src/main/java/en/ratings/own/my/utility/PasswordUtility.java
+++ src/main/java/en/ratings/own/my/utility/PasswordUtility.java
@@ -6,18 +6,18 @@ import static en.ratings.own.my.utility.StringUtility.EMPTY_STRING;
 
 public class PasswordUtility {
 
-    public static final int PASSWORD_MINIMUM_LENGTH = 6;
+    public static final int PASSWORD_MINIMUM_LENGTH = 8;
     public static final int PASSWORD_MAXIMUM_LENGTH = 64;
-    public static final String AT_LEAST_ONE_ENGLISH_UPPER_CASE_LETTER_REGEX = "^(?=.*?[A-Z])$";
-    public static final String AT_LEAST_ONE_ENGLISH_LOWER_CASE_LETTER_REGEX = "^(?=.*?[a-z])$";
-    public static final String AT_LEAST_ONE_DIGIT_REGEX = "^(?=.*?[0-9])$";
-    public static final String AT_LEAST_ONE_VALID_SPECIAL_CHARACTER_REGEX = "^(?=.*?[#%&@^`~])$";
+    public static final String AT_LEAST_ONE_ENGLISH_UPPER_CASE_LETTER_REGEX = "^.*[A-Z].*";
+    public static final String AT_LEAST_ONE_ENGLISH_LOWER_CASE_LETTER_REGEX = ".*[a-z].*";
+    public static final String AT_LEAST_ONE_DIGIT_REGEX = ".*[0-9].*";
+    public static final String AT_LEAST_ONE_VALID_SPECIAL_CHARACTER_REGEX = ".*[#%&@^`~].*";
 
     public static final ArrayList<String> LIST_OF_VALID_SPECIAL_CHARACTERS = getValidSpecialCharacters();
 
     public static ArrayList<String> getValidSpecialCharacters() {
-        String prefix = "^(?=.*?[";
-        String suffix = "])$";
+        String prefix = ".*[";
+        String suffix = "].*";
         String specialCharacters = AT_LEAST_ONE_VALID_SPECIAL_CHARACTER_REGEX.replace(prefix, EMPTY_STRING).
                 replace(suffix, EMPTY_STRING);
         int length = specialCharacters.length();
