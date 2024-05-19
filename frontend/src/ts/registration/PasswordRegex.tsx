62c0913361918ea15ff42c0e548c0f5a104784d5
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-19 20:01:55 +0200

    MOR-12: outsource password regex

---
+++ frontend/src/ts/registration/PasswordRegex.tsx
@@ -0,0 +1,41 @@
+export class PasswordRegex {
+    private atLeastOneDigitRegExp: RegExp;
+    private atLeastOneEnglishUpperCaseLetterRegExp: RegExp;
+    private atLeastOneEnglishLowerCaseLetterRegExp: RegExp;
+    private atLeastOneValidSpecialCharacterRegExp: RegExp;
+    private enumerationOfValidSpecialCharacters: string[];
+
+    constructor(atLeastOneDigitRegex: string, atLeastOneEnglishUpperCaseLetterRegex: string,
+                atLeastOneEnglishLowerCaseLetterRegex: string, atLeastOneValidSpecialCharacterRegex: string,
+                enumerationOfValidSpecialCharacters: string[]) {
+        this.atLeastOneDigitRegExp = new RegExp(atLeastOneDigitRegex);
+        this.atLeastOneEnglishUpperCaseLetterRegExp = new RegExp(atLeastOneEnglishUpperCaseLetterRegex);
+        this.atLeastOneEnglishLowerCaseLetterRegExp = new RegExp(atLeastOneEnglishLowerCaseLetterRegex);
+        this.atLeastOneValidSpecialCharacterRegExp = new RegExp(atLeastOneValidSpecialCharacterRegex);
+        this.enumerationOfValidSpecialCharacters = enumerationOfValidSpecialCharacters;
+    }
+
+    public checkPassword = (password: string, setIsPasswordValid: any, setPasswordErrors: any)=> {
+        let passwordErrors: string[] = [];
+        let isPasswordValid: boolean = true;
+        if (!this.atLeastOneDigitRegExp.test(password)) {
+            isPasswordValid = false;
+            passwordErrors.push("Your password needs minimum one digit");
+        }
+        if (!this.atLeastOneEnglishUpperCaseLetterRegExp.test(password)) {
+            isPasswordValid = false;
+            passwordErrors.push("Your password needs minimum one English uppercase letter");
+        }
+        if (!this.atLeastOneEnglishLowerCaseLetterRegExp.test(password)) {
+            isPasswordValid = false;
+            passwordErrors.push("Your password needs minimum one English lowercase letter");
+        }
+        if(!this.atLeastOneValidSpecialCharacterRegExp.test(password)) {
+            isPasswordValid = false;
+            passwordErrors.push("Your password needs minimum one of the following special characters: " +
+                this.enumerationOfValidSpecialCharacters);
+        }
+        setIsPasswordValid(isPasswordValid);
+        setPasswordErrors(passwordErrors);
+    };
+};
