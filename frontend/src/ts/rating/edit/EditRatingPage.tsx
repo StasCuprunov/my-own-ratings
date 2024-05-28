de4b406a861e41ee4a0cb8de512863d297039159
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-28 16:36:06 +0200

    MOR-16: add website structure for changing name and description

--- frontend/src/ts/rating/edit/EditRatingPage.tsx
+++ frontend/src/ts/rating/edit/EditRatingPage.tsx
@@ -1,9 +1,41 @@
 import {FunctionComponent} from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-export const EditRatingPage: FunctionComponent<any> = () => {
+export const EditRatingPage: FunctionComponent<any> = ({oldName, oldDescription, handleSubmit}) => {
     return (
         <div>
-            <h1>Edit your rating</h1>
+            <h1>Edit the rating {oldName}</h1>
+            <form onSubmit={handleSubmit}>
+                <TableContainer component={Paper}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Attribute</TableCell>
+                                <TableCell>Old value</TableCell>
+                                <TableCell>New value</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell>LABEL Name</TableCell>
+                                <TableCell>{oldName}</TableCell>
+                                <TableCell>INPUT Name</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell>LABEL Description</TableCell>
+                                <TableCell>{oldDescription}</TableCell>
+                                <TableCell>INPUT Description</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </form>
         </div>
     );
 };
