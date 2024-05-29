040c75aa93d98068e56b881083d0d8ba1f7df29a
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-29 11:33:51 +0200

    MOR-16: fix LoadEditRating because rollback

--- frontend/src/ts/rating/edit/LoadEditRating.tsx
+++ frontend/src/ts/rating/edit/LoadEditRating.tsx
@@ -2,7 +2,6 @@ import {FunctionComponent} from "react";
 import {LoadComponentHandling} from "../../component/LoadComponentHandling";
 import {EditRating} from "./EditRating";
 import {useParams} from "react-router-dom";
-import {useRating} from "../useRating";
 
 export const LoadEditRating: FunctionComponent<any> = () => {
     const {data, error} = {data: "", error: null};
