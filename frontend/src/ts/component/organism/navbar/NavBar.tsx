c9b5021c0081b2591ba9e29dd0fc77b7a307df6c
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-05-31 11:18:07 +0200

    MOR-43: expand navbar, add login button in NotAuthorizedPage

--- frontend/src/ts/component/organism/navbar/NavBar.tsx
+++ frontend/src/ts/component/organism/navbar/NavBar.tsx
@@ -1,16 +1,22 @@
 import {FunctionComponent} from "react";
 import {useAuth} from "../../../context/AuthContext";
 import {ButtonLink} from "../../atom/button/link/ButtonLink";
-import {getLogoutObject} from "./NavBarFunctions";
+import {getCreateRatingProps, getLogoutProps, getStartPageProps} from "./NavBarFunctions";
 
-const logout: any = getLogoutObject();
+const logout: any = getLogoutProps();
+const startPage: any = getStartPageProps();
+const createRating: any = getCreateRatingProps();
 
 export const NavBar: FunctionComponent<any> = () => {
     const {authenticated} = useAuth();
     return (
         <div>
             {authenticated &&
-                <ButtonLink props={logout}/>
+                <div>
+                    <ButtonLink props={startPage}/>
+                    <ButtonLink props={createRating}/>
+                    <ButtonLink props={logout}/>
+                </div>
             }
         </div>
     );
--- frontend/src/ts/general-page/NotAuthorizedPage.tsx
+++ frontend/src/ts/general-page/NotAuthorizedPage.tsx
@@ -1,10 +1,17 @@
 import {FunctionComponent} from "react";
+import {ButtonLink} from "../component/atom/button/link/ButtonLink";
+import {WEBSITE_ROUTING_LOGIN} from "../constant/routing/WebsiteRoutingConstants";
 
+const login: any = {
+    to: WEBSITE_ROUTING_LOGIN,
+    text: "Login"
+};
 export const NotAuthorizedPage: FunctionComponent<any> = () => {
     return (
         <div>
             <h1>Not authorized</h1>
             <p>You have to log in to use this page.</p>
+            <ButtonLink props={login}/>
         </div>
     );
 };
