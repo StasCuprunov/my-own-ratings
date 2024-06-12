de50ae28d826fff7b28796f3597c57924b4c5861
Stas Cuprunov <stas.cuprunov@t-online.de>
2024-06-12 16:39:39 +0200

    MOR-38: implement backend error handling for create/edit rating entry with too long name

--- src/main/java/en/ratings/own/my/constant/ExceptionConstants.java
+++ src/main/java/en/ratings/own/my/constant/ExceptionConstants.java
@@ -37,6 +37,7 @@ public final class ExceptionConstants {
     // For RatingEntryFailedException
     public static final String KEY_RATING_ENTRY_BY_ID_NOT_FOUND = "ratingEntryByIdNotFound";
     public static final String KEY_RATING_ENTRY_NAME_IS_EMPTY = "ratingEntryNameIsEmpty";
+    public static final String KEY_RATING_ENTRY_NAME_IS_TOO_LONG = "ratingEntryNameIsTooLong";
     public static final String KEY_RATING_ENTRY_NAME_ALREADY_USED_IN_RATING = "ratingEntryNameAlreadyUsedInRating";
     public static final String KEY_RATING_ENTRY_VALUE_HAS_TOO_MANY_DECIMAL_DIGITS =
             "ratingEntryValueHasTooManyDecimalDigits";
